6932672aaacd39168cc1fc04ff87487ef1b10a5e
bobertsworld <bobertsworld@gmail.com>
2022-12-19 23:49:29 -0800

    commit tree e22c53edc25bc5ffbc6b87e936f9126961b97b94

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import NavBar from "./NavBar";
 import Home from "./Home";
+import NavBar from "./NavBar";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
+
+
 import user from "../data/user";
 console.log(user);
 
@@ -11,10 +12,16 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city = {user.city} color = {user.color}/>
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
+
+
+
+
+
+
 
 export default App;
